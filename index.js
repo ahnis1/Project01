2bc92c57d3c4464220fa46f3b513304ab0c869e7
ahnis1 <110741951+ahnis1@users.noreply.github.com>
2023-03-03 11:40:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const express = require('express')
-const app = express()
-
-const port = process.env.PORT || 3030;
-
-app.get('/', (req, res) => res.send('Hello World! This is Nodejs running on Ec2 Docker instance - :) Demo complete'))
-
-app.listen(port, (err) => {
-    if (err) {
-      console.log('Error::', err);
-    }
-      console.log(`Nodejs app listening on port ${port}`);
-  });
+<?php
+echo "Hello World!";
+?>
