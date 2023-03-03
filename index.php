419d9d2561da1c1cb0103f81b71a8a6cd6a17082
ahnis1 <110741951+ahnis1@users.noreply.github.com>
2023-03-03 13:55:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-echo "Hello World!";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Application</title>
+</head>
+<body>
+	<h1>Hello, PHP!</h1>
+	<p>This is a simple PHP application running on Apache HTTP Server.</p>
+	<p>The current date and time is: <?php echo date('Y-m-d H:i:s'); ?></p>
+</body>
+</html>
